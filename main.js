2e07766afe66d7fae178847a1590cec0f937bd26
Bolek <103572991+BoloMasta@users.noreply.github.com>
2022-11-02 16:56:31 +0100

    final comit
    style and build js
    all feats

--- main.js
+++ main.js
@@ -1,1 +1,48 @@
-console.log("KOKODS");
+const result = document.querySelector(".result");
+const playerScore = document.querySelector(".player__score--value");
+const playerButtons = document.querySelectorAll(".playerBtn");
+const computerScore = document.querySelector(".computer__score--value");
+const computerButtons = document.querySelectorAll(".computer__choice--button");
+const resetBtn = document.querySelector(".resetBtn");
+
+playerButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    computerButtons.forEach((button) => {
+      button.classList.remove("compResultActive");
+    });
+
+    const playerChoice = button.value;
+    const computerChoice = computerButtons[Math.floor(Math.random() * 3)].value;
+
+    const findBtn = Array.from(computerButtons).find(
+      (btn) => btn.value === computerChoice
+    );
+    findBtn.classList.add("compResultActive");
+    setTimeout(() => {
+      findBtn.classList.remove("compResultActive");
+    }, 2000);
+
+    if (playerChoice === computerChoice) {
+      result.textContent = "Draw";
+    } else if (
+      (playerChoice === "rock" && computerChoice === "scissors") ||
+      (playerChoice === "paper" && computerChoice === "rock") ||
+      (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+      result.textContent = "You won";
+      playerScore.textContent = parseInt(playerScore.textContent) + 1;
+    } else {
+      result.textContent = "You lost";
+      computerScore.textContent = parseInt(computerScore.textContent) + 1;
+    }
+
+    resetBtn.style.display = "block";
+  });
+});
+
+resetBtn.addEventListener("click", () => {
+  playerScore.textContent = 0;
+  computerScore.textContent = 0;
+  result.textContent = "New game";
+  resetBtn.style.display = "none";
+});
